f4c02961732de37a496a339e01f726105571b1af
Mansoor Nasir <taimur.aamer@gmail.com>
2022-04-20 13:48:47 +0500

    apis

---
+++ javascript/api-playground/apis.js
@@ -0,0 +1,23 @@
+// The following returns address on the basis of coordinates
+
+// https://us1.locationiq.com/v1/reverse.php?key=pk.2ce05a50d1ca31e503b2a60e82d6a395&lat=34.0151&lon=71.5249&format=json
+
+// https://www.mapquestapi.com/geocoding/v1/reverse?key=Nnxzvmg08vlDM8y1cgiA2pNcEgG2P0Jd&location=34.0000%2C71.4757&outFormat=json&thumbMaps=false
+
+//  https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=34.0151&longitude=71.5249&localityLanguage=en
+
+
+// The following will return information about a fruit :)
+// https://www.fruityvice.com/api/fruit/all
+// https://www.fruityvice.com/api/fruit/apple
+
+
+// NYTimes APIs
+// Key: t7nDQ8uP9GAjcnPpAbWPmR5Q7yAis9Mf
+// Sample Call: https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=t7nDQ8uP9GAjcnPpAbWPmR5Q7yAis9Mf
+// https://developer.nytimes.com/docs/movie-reviews-api/1/overview
+// https://developer.nytimes.com/ for more apis
+
+
+// Covid Cases
+// https://covid-api.mmediagroup.fr/v1/cases
---
+++ javascript/weather-app/weather.js
@@ -0,0 +1,2 @@
+// The following will return all weather information along with other details
+// http://api.weatherapi.com/v1/current.json?q=48.8567,2.3508&key=1db4640dedd8405b9bd195826221804
